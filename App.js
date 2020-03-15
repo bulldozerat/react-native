984ca6a7e019d616e5c66e9919a7cdc2818f05fb
bulldozerat <bulldozerat@gmail.com>
2020-03-15 14:19:42 +0200

    Fonts added

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+//Other
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
 
 // Components
 import Home from './screens/Home';
 import About from './screens/About';
 import ReviewDetails from './screens/ReviewDetails';
 
+const getFonts = () => {
+  return Font.loadAsync({
+    'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+    'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
+  });
+};
+
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Home />
-      <About />
-      <ReviewDetails />
-    </View>
-  );
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if (fontsLoaded) {
+    return (
+      <View style={styles.container}>
+        <Home />
+        <About />
+        <ReviewDetails />
+      </View>
+    );
+  }
+
+  return <AppLoading startAsync={getFonts} onFinish={() => setFontsLoaded(true)} />;
 }
 
 const styles = StyleSheet.create({
--- screens/Home.js
+++ screens/Home.js
@@ -3,13 +3,17 @@ import { StyleSheet, View, Text } from 'react-native';
 
 const Home = () => (
   <View style={styles.container}>
-    <Text>Home Screen</Text>
+    <Text style={styles.titleText}>Home Screen</Text>
   </View>
 );
 
 const styles = StyleSheet.create({
   container: {
     padding: 24
+  },
+  titleText: {
+    fontFamily: 'nunito-bold',
+    fontSize: 18
   }
 });
 
