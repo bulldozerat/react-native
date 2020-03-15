f28423d335a55c515b20b0b731cfbee893ef55be
bulldozerat <bulldozerat@gmail.com>
2020-03-15 14:33:30 +0200

    Global styles added

--- screens/Home.js
+++ screens/Home.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { globalStyles } from '../styles/global';
 
 const Home = () => (
   <View style={styles.container}>
-    <Text style={styles.titleText}>Home Screen</Text>
+    <Text style={globalStyles.titleText}>Home Screen</Text>
   </View>
 );
 
 const styles = StyleSheet.create({
   container: {
     padding: 24
-  },
-  titleText: {
-    fontFamily: 'nunito-bold',
-    fontSize: 18
   }
 });
 
